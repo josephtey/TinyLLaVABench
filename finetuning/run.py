cbb3935989f269757992f8b6c9c41662b5c1eda9
Joseph Tey <jjttey@gmail.com>
2024-05-16 20:25:04 -0700

    done

--- finetuning/run.py
+++ finetuning/run.py
@@ -7,7 +7,7 @@ pretrained_model_path = (
     "/piech/u/joetey/TinyLLaVABench/finetuning/pre-trained/TinyLLaVA-3.1B"
 )
 lora_model_path = "/piech/u/joetey/TinyLLaVABench/finetuning/output/TinyLLaVA-3.1B-lora"
-image_path = "images/2Ap9EEd8geGLDT6UQpjHGd.jpg"  # Update with your image path
+image_file = "images/2Ap9EEd8geGLDT6UQpjHGd.jpg"  # Update with your image path
 # image_file = "https://llava-vl.github.io/static/images/view.jpg"
 prompt = "What is this?"
 
