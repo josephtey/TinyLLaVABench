55da9b8375da9b66319b68f55827e0b408138e0b
Joseph Tey <jjttey@gmail.com>
2024-05-19 12:31:28 -0700

    fixed the stupidest bug

--- custom/evals/eval_mathverse.sh
+++ custom/evals/eval_mathverse.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # data
-DATA_FILE="../../../multimodal-reasoning/dataset/eval/mathverse/mathverse_visual_only.json"
-IMAGE_FOLDER="../../../multimodal-reasoning/dataset/eval/mathverse/images"
+DATA_FILE="./../../../multimodal-reasoning/dataset/eval/mathverse/mathverse_visual_only.json"
+IMAGE_FOLDER="./../../../multimodal-reasoning/dataset/eval/mathverse/images"
 
 # pretrained model
 MODEL_BASE="./../checkpoints/pre-trained/TinyLLaVA-3.1B"
@@ -11,11 +11,11 @@ MODEL_BASE="./../checkpoints/pre-trained/TinyLLaVA-3.1B"
 MODEL_PATH="./../checkpoints/fine-tuned/TinyLLaVA-3.1B-lora"
 
 # results file
-RESULTS_FILE="../../../multimodal-reasoning/results/files/results_$(date +%Y%m%d_%H%M%S).json"
+RESULTS_FILE="./../../../multimodal-reasoning/results/files/results_$(date +%Y%m%d_%H%M%S).json"
 
 python -m eval \
-    --image-folder $IMAGE_FOLDER$ \
-    --data-file $DATA_FILE$ \
+    --image-folder $IMAGE_FOLDER \
+    --data-file $DATA_FILE \
     --model-base $MODEL_BASE \
     --model-path $MODEL_PATH \
     --results-file $RESULTS_FILE \
