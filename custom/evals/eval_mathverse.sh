18b75c61206b4ade8e6ad6dd0cf6bbb1779fabf8
Joseph Tey <jjttey@gmail.com>
2024-05-19 11:37:16 -0700

    new structure

---
+++ custom/evals/eval_mathverse.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# data
+DATA_FILE = "../multimodal-reasoning/dataset/eval/mathverse/mathverse_visual_only.json"
+IMAGE_FOLDER = "../multimodal-reasoning/dataset/eval/mathverse/images"
+
+# pretrained model
+MODEL_BASE="custom/checkpoints/pre-trained/TinyLLaVA-3.1B"
+
+# finetuned model
+MODEL_PATH="custom/checkpoints/finetuned/TinyLLaVA-3.1B-lora"
+
+# results file
+RESULTS_FILE="../multimodal-reasoning/results/files/results_$(date +%Y%m%d_%H%M%S).json"
+
+python -m eval.py \
+    --image_folder $IMAGE_FOLDER$ \
+    --data-file $DATA_FILE$ \
+    --model-base $MODEL_BASE \
+    --model-path $MODEL_PATH \
+    --temperature 0 \
+    --conv-mode phi
