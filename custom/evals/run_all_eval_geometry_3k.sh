fd9268c053218d5ae32b3f32f237b61e4c0c819e
Joseph Tey <jjttey@gmail.com>
2024-05-21 02:32:55 -0700

    fixed

--- custom/evals/run_all_eval_geometry_3k.sh
+++ custom/evals/run_all_eval_geometry_3k.sh
@@ -2,15 +2,15 @@
 
 echo "Starting baseline direct evaluation..."
 # Run baseline direct evaluation
-./eval_geometry_3k_baseline_direct.sh
+bash eval_geometry_3k_baseline_direct.sh
 echo "Finished baseline direct evaluation."
 
 echo "Starting baseline CoT evaluation..."
 # Run baseline CoT evaluation
-./eval_geometry_3k_baseline_cot.sh
+bash eval_geometry_3k_baseline_cot.sh
 echo "Finished baseline CoT evaluation."
 
 echo "Starting finetuned evaluation..."
 # Run finetuned evaluation
-./eval_geometry_3k_finetuned.sh
+bash eval_geometry_3k_finetuned.sh
 echo "Finished finetuned evaluation."
