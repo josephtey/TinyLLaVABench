37fffce3485211f228c0c1bf34cf6e6f24b45b67
Joseph Tey <jjttey@gmail.com>
2024-06-01 21:18:55 -0700

    done

--- custom/evals/eval_geometry_3k_baseline_direct.sh
+++ custom/evals/eval_geometry_3k_baseline_direct.sh
@@ -8,13 +8,13 @@ IMAGE_FOLDER="./../../../multimodal-reasoning/dataset/eval/geometry_3k/images"
 MODEL_PATH="./../checkpoints/pre-trained/TinyLLaVA-3.1B"
 
 # results file
-RESULTS_FILE="./../../../multimodal-reasoning/results/files/results_geometry_3k_baseline_direct_$(date +%Y%m%d_%H%M%S).json"
+RESULTS_FOLDER="./../../../multimodal-reasoning/results/files/results_geometry_3k_baseline_direct_$(date +%Y%m%d_%H%M%S)"
 
 python -m eval_geometry_3k \
     --image-folder $IMAGE_FOLDER \
     --data-file $DATA_FILE \
     --model-path $MODEL_PATH \
-    --results-file $RESULTS_FILE \
+    --results-folder $RESULTS_FOLDER \
     --temperature 0 \
     --conv-mode phi \
     --baseline \
